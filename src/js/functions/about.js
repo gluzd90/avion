1ebf8a1b915fe883e8b5c1a833b66389e7f9b4ee
Elena Gluzdova <elenagavrilova003@gmail.com>
2024-07-09 15:10:34 +0300

    Update 2024-07-09T12:10:13.161Z

--- src/js/functions/about.js
+++
@@ -1,8 +0,0 @@
-const noticeEl = document.querySelector('.notice');
-
-if(noticeEl) {
-  const noticeBtn = document.querySelector('.notice__btn');
-  noticeBtn.addEventListener('click', () => {
-    noticeEl.classList.add('notice--hidden')
-  })
-}
